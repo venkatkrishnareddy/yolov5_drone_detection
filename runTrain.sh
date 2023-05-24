701910dcf2e9b7d75924e63abed5b2782718bd11
MM <mutlu.mete@tamuc.edu>
2023-05-23 23:19:12 -0500

    runTrain csci01 update

--- runTrain.sh
+++ runTrain.sh
@@ -9,3 +9,6 @@ python /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/train.py --epoc
 python /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/train.py --epochs 100 --workers 4 --device cpu --batch-size 32 --data /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/data/droneDataset.yaml --img 640 640 --cfg /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/cfg/training/yolov7-tinyVenkatV2-2.2MB.yaml --weights /scratch/user/u.mm115921/yolov5_drone_detection/yolo_tiny_venkat_may13.pt --name yolov7tinymay16 --hyp /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/data/hyp.scratch.tiny.yaml 
 
 
+python /home/mmete/yolov5_drone_detection/yolo7tiny/train.py --epochs 100 --workers 4 --device 0 --batch-size 32 --data /home/mmete/yolov5_drone_detection/yolo7tiny/data/droneDataset.yaml --img 640 640 --cfg /home/mmete/yolov5_drone_detection/yolo7tiny/cfg/training/yolov7-tinyVenkatV2-2.2MB.yaml --weights /home/mmete/yolov5_drone_detection/yolo7tiny/yolo_tiny_Venkat_May13.pt --name yolov7tinyMay16 --hyp /home/mmete/yolov5_drone_detection/yolo7tiny/data/hyp.scratch.tiny.yaml 
+
+
