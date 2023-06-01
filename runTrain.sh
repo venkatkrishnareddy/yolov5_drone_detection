19f067b2613a1898ca62266a9db1b994e6e45745
MM <onescore@github.com>
2023-06-01 09:55:17 -0500

    run Train update

--- runTrain.sh
+++ runTrain.sh
@@ -1,6 +1,11 @@
+conda env list
 conda activate yolo
+cd yolo7tiny
 
-conda activate drone
+# Run On CPU 
+python /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/train.py --epochs 100 --workers 4 --device cpu --batch-size 32 --data /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/data/droneDataset.yaml --img 640 640 --cfg /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/cfg/training/yolov7-tinyVenkatV2-2.2MB.yaml --weights /scratch/user/u.mm115921/yolov5_drone_detection/yolo_tiny_venkat_may13.pt --name yolov7tinymay16 --hyp /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/data/hyp.scratch.tiny.yaml 
+
+
 
 cd yolo7tiny
 
@@ -9,7 +14,6 @@ python /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/train.py --epoc
 
 python /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/train.py --epochs 100 --workers 4 --device 0 --batch-size 32 --data /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/data/droneDataset.yaml --img 640 640 --cfg /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/cfg/training/yolov7-tinyVenkatV2-2.2MB.yaml --weights /scratch/user/u.mm115921/yolov5_drone_detection/yolo_tiny_venkat_may13.pt --name yolov7tinyMay16 --hyp /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/data/hyp.scratch.tiny.yaml 
 
-python /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/train.py --epochs 100 --workers 4 --device cpu --batch-size 32 --data /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/data/droneDataset.yaml --img 640 640 --cfg /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/cfg/training/yolov7-tinyVenkatV2-2.2MB.yaml --weights /scratch/user/u.mm115921/yolov5_drone_detection/yolo_tiny_venkat_may13.pt --name yolov7tinymay16 --hyp /scratch/user/u.mm115921/yolov5_drone_detection/yolo7tiny/data/hyp.scratch.tiny.yaml 
 
 
 python /home/mmete/yolov5_drone_detection/yolo7tiny/train.py --epochs 100 --workers 4 --device 0 --batch-size 32 --data /home/mmete/yolov5_drone_detection/yolo7tiny/data/droneDataset.yaml --img 640 640 --cfg /home/mmete/yolov5_drone_detection/yolo7tiny/cfg/training/yolov7-tinyVenkatV2-2.2MB.yaml --weights /home/mmete/yolov5_drone_detection/yolo_tiny_venkat_may13.pt --name yolov7tinyMay16 --hyp /home/mmete/yolov5_drone_detection/yolo7tiny/data/hyp.scratch.tiny.yaml 
